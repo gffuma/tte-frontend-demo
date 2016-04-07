b4121e4199867de9ca2a2dfeb1d476fe252c6c63
gffuma <fumagalli.gf@gmail.com>
2016-04-07 11:16:44 +0200

    Better webpack

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 //import './style.scss';
 import 'babel-polyfill';
-import { createStore, applyMiddleware } from 'redux';
 import React from 'react';
 import ReactDom from 'react-dom';
-import rootReducer from './reducers';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
+import configureStore from './store/configureStore';
 import App from './containers/App';
 import { loadCutters, loadGeometries, loadSearchFamilies } from './actions';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk, createLogger())
-);
+const store = configureStore();
 
-
+// Loading initial data...
 store.dispatch(loadCutters());
 store.dispatch(loadGeometries());
 store.dispatch(loadSearchFamilies());
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,15 +2,25 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: './src/main.js',
+  devtool: 'source-map',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/tte-frontend-demo/dist/'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
   ],
   module: {
     loaders: [
