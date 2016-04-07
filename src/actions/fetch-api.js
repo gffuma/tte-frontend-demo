29eb8ea3ef4bc3615d5bae33e9f0db01b6d730a5
gffuma <fumagalli.gf@gmail.com>
2016-04-07 12:27:58 +0200

    SSL api url

--- src/actions/fetch-api.js
+++ src/actions/fetch-api.js
@@ -1,6 +1,6 @@
 import fetch from 'isomorphic-fetch';
 
-const API_URL = 'http://tte.trizero.biz';
+const API_URL = 'https://tte.trizero.biz';
 // i love my token <3
 const TOKEN = 'MTQ1ODMxOTU2OA==.8LfkVjFEq4NAtLImtAZfjruCAPaajyU6o6lHw1mUnRkJGEMzUk2hRnWt5uKxP1l8UwFgkFrL1X3DfMFKkav';
 
