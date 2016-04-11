f74d602fcf04435323a947ae08b6cf1fc3756884
gffuma <fumagalli.gf@gmail.com>
2016-04-11 22:16:57 +0200

    Redux Routing middleware

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { routerMiddleware, push } from 'react-router-redux';
+import { hashHistory } from 'react-router';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, createLogger())
+    applyMiddleware(thunk, routerMiddleware(hashHistory), createLogger())
   );
 };
--- src/store/configureStore.prod.js
+++ src/store/configureStore.prod.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { routerMiddleware, push } from 'react-router-redux';
+import { hashHistory } from 'react-router';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, routerMiddleware(hashHistory))
   );
 };
